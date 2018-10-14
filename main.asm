8e62d83b1e97f921cc473869f4c87808b68346c6
Diango <diangogavidia@gmail.com>
2018-10-14 17:54:45 -0400

    Se agregó la dinámica del juego

--- main.asm
+++ main.asm
@@ -29,16 +29,29 @@ CONTA_1	 EQU 0x21
 BIT	 EQU 0x22
 TECLA	 EQU 0x23
 LAST_TECLA EQU 0x24
+RESPUESTA EQU 0x25
+CORRECTO EQU 0x26
+CONT_WIN EQU 0x27
 	ORG H'0'
 	GOTO INICIO
 
 CONVERT_HEX
 	ADDWF PCL,F
-	DT .1,.2,.3,.4,.5
-	DT .6,.7,.8,.9,.10
-	DT .11,.12,.11,.12
-	DT .13,.14,.14,.16
+	DT .1,  .2, .3, .4, .5
+	DT .6,  .7, .8, .9,.10
+	DT .11,.12,.13,.14,.15
+	DT .16,.17,.18,.19,.20
+	DT .21,.22,.23,.24,.25
 END_CONVERT_HEX
+	
+RESPUESTAS_1 
+	ADDWF PCL,F
+	DT .1,.7,.23,.14,.25
+	DT .21,.17,.3,.24,.10
+	DT .16,.22,.3,.9,.5
+	DT .16,.22,.3,.9,.5
+	DT .16,.22,.3,.9,.5
+END_RESPUESTAS_1
 	
 RETARDO_20MS
 	BANK0
@@ -65,43 +78,103 @@ INICIO
 	CLRF PORTA
 	CLRF PORTD
 	CLRF PORTE
+	CLRF CONT_WIN
 	CALL TecladoInicializa
+	
 LOOP	
-
+	; Fila 1
 	BTFSC PORTC,0
 	GOTO LOOP
-	CALL READ_HEX
+	CALL VALIDATE_ANSWER
+	BCF STATUS,Z
+	MOVLW .1
+	SUBWF CORRECTO
+	BTFSS STATUS,Z
+	GOTO LOOP
+	CLRF CORRECTO
+	INCF CONT_WIN
+	MOVLW B'00000001'
 	MOVWF PORTD
-	CALL Teclado_EsperaDejePulsar
+LOOP2
+	; Fila 2
+	BTFSC PORTC,1
+	GOTO LOOP2
+	CALL VALIDATE_ANSWER
+	BCF STATUS,Z
+	MOVLW .1
+	SUBWF CORRECTO
+	BTFSS STATUS,Z
+	GOTO LOOP2
+	CLRF CORRECTO
+	INCF CONT_WIN
+	MOVLW B'00000011'
+	MOVWF PORTD
+	
+LOOP3
+	; Fila 3
+	BTFSC PORTC,2
+	GOTO LOOP3
+	CALL VALIDATE_ANSWER
+	BCF STATUS,Z
+	MOVLW .1
+	SUBWF CORRECTO
+	BTFSS STATUS,Z
+	GOTO LOOP3
+	CLRF CORRECTO
+	INCF CONT_WIN
+	MOVLW B'00000111'
+	MOVWF PORTD
+
+LOOP4
+	; Fila 4
+	BTFSC PORTC,3
+	GOTO LOOP4
+	CALL VALIDATE_ANSWER
+	BCF STATUS,Z
+	MOVLW .1
+	SUBWF CORRECTO
+	BTFSS STATUS,Z
+	GOTO LOOP4
+	CLRF CORRECTO
+	INCF CONT_WIN
+	MOVLW B'00001111'
+	MOVWF PORTD	
+LOOP5
+	; Fila 5
+	BTFSC PORTC,4
+	GOTO LOOP5
+	CALL VALIDATE_ANSWER
+	BCF STATUS,Z
+	MOVLW .1
+	SUBWF CORRECTO
+	BTFSS STATUS,Z
+	GOTO LOOP5
+	CLRF CORRECTO
+	INCF CONT_WIN
+	MOVLW B'00011111'
+	MOVWF PORTD
+
+CLEAR
+	CLRF PORTD
 	GOTO LOOP
 	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-;==============================================================================	
+;==============================================================================
+;VALIDA SI LA RESPUESTA ES CORRECTA
+VALIDATE_ANSWER	
+	CALL READ_HEX
+	MOVWF RESPUESTA
+	CALL Teclado_EsperaDejePulsar
+	MOVF CONT_WIN,W
+	CALL RESPUESTAS_1
+	SUBWF RESPUESTA
+	BTFSS STATUS,Z
+	RETURN
+	MOVLW .1
+	MOVWF CORRECTO
+	RETURN
+;==============================================================================
+;==============================================================================
+;LEER EL VALOR PULSADO
 READ_HEX
 	CALL Teclado_LeeOrdenTecla
 	BTFSS STATUS,C
@@ -111,7 +184,7 @@ READ_HEX
 READ_HEX_END
 	RETURN
 ;===============================================================================
-	
+;INICIALIZAR EL TECLADO
 TecladoInicializa
 	BANK1
 	MOVLW B'00000000'
@@ -122,21 +195,23 @@ TecladoInicializa
 	MOVLW .25
 	MOVWF LAST_TECLA
 	RETURN
-
+;===============================================================================
+;ANTIREBOTE
 Teclado_EsperaDejePulsar
 	BANK0
-	MOVLW 0x00
-	MOVWF PORTB
+	MOVLW 0x1F
+	MOVWF PORTC
 Teclado_SigueEsperando
 	CALL RETARDO_20MS
-	MOVF PORTB,W
-	SUBLW 0x00
+	MOVF PORTC,W
+	SUBLW 0x1F
 	BTFSS STATUS,Z
 	GOTO Teclado_SigueEsperando
+	CLRF PORTB
 	RETURN
-
+;===============================================================================
+;SCAN TECLAS
 Teclado_LeeOrdenTecla
-	
 	BANK0
 	CLRF TECLA
 	MOVLW B'11111110'
